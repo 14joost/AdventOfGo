f31e96df455942ecd563477452af36beb40740d7
14joost <joostsessink@gmail.com>
2024-12-18 20:37:30 +0100

    Intermediate commit and push

---
+++ challenges/day1.go
@@ -0,0 +1,11 @@
+package challenges
+
+import "fmt"
+
+func Day1(inputList []string) {
+
+	//fmt.Print(inputList)
+	for _, line := range inputList {
+		fmt.Println(line)
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"AdventOfGo/challenges"
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
+func init() {
+	// This function will be executed before the main function
+	// It is used to initialize the program
+}
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	day, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
 	}
+	test, err := strconv.ParseBool(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Solving challenge for day %d! Test input = %t\n", day, test)
+
+	inputFile := fmt.Sprintf("input_day%d", day)
+	inputList := make([]string, 0)
+	var testStr string
+	if test {
+		testStr = "_test"
+	}
+	file, err := os.Open(fmt.Sprintf("inputFiles/%s%s.txt", inputFile, testStr))
+	scanner := bufio.NewScanner(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for scanner.Scan() {
+		text := scanner.Text()
+		inputList = append(inputList, text)
+		//fmt.Println(text)
+	}
+	challenges.Day1(inputList)
 }
