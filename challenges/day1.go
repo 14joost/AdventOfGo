18938e205bf03845eb7dc7e78125c51396b320cd
14joost <joostsessink@gmail.com>
2024-12-22 21:37:16 +0100

    Day1 part 2

--- challenges/day1.go
+++ challenges/day1.go
@@ -3,11 +3,13 @@ package challenges
 import (
 	"AdventOfGo/utils"
 	"fmt"
+	"log"
 	"math"
 	"sort"
+	"strconv"
 )
 
-func Day1(inputList []string) {
+func Day1Part1(inputList []string) {
 
 	//fmt.Print(inputList)
 	var leftSlice, rightSlice []string
@@ -36,4 +38,41 @@ func Day1(inputList []string) {
 		diffSum += int(math.Abs(float64(rightSliceInt[i] - leftSliceInt[i])))
 	}
 	fmt.Println("Sum of differences: ", diffSum)
+}
+
+func Day1Part2(inputList []string) {
+	//fmt.Print(inputList)
+	leftMap := make(map[int]int)
+	rightMap := make(map[int]int)
+	for _, line := range inputList {
+		sliceNoWhitespace := utils.SplitByWhitespace(line)
+		left, err := strconv.Atoi(sliceNoWhitespace[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		right, err := strconv.Atoi(sliceNoWhitespace[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		checkAndUpdateMap(leftMap, left)
+		checkAndUpdateMap(rightMap, right)
+	}
+
+	fmt.Println("Left map: ", leftMap)
+	fmt.Println("Right map: ", rightMap)
+
+	similarityScore := 0
+
+	for lKey, lValue := range leftMap {
+		similarityScore += lKey * rightMap[lKey] * lValue
+	}
+	fmt.Println("Similarity score: ", similarityScore)
+}
+
+func checkAndUpdateMap(m map[int]int, key int) {
+	if _, ok := m[key]; ok {
+		m[key]++
+	} else {
+		m[key] = 1
+	}
 }
--- main.go
+++ main.go
@@ -9,29 +9,50 @@ import (
 	"strconv"
 )
 
-func init() {
-	// This function will be executed before the main function
-	// It is used to initialize the program
-}
 func main() {
-	var day int
+	var day, part int
 	var test bool
 	var err error
-	if len(os.Args) < 2 {
-		fmt.Println("No arguments provided. Defaulting to day 1 and test input")
+	if len(os.Args) < 4 {
+		fmt.Println("No sufficient arguments provided. Defaulting to day 1, puzzle 1 and test input")
 		day = 1
+		part = 1
 		test = true
 	} else {
 		day, err = strconv.Atoi(os.Args[1])
 		if err != nil {
 			log.Fatal(err)
 		}
-		test, err = strconv.ParseBool(os.Args[2])
+		part, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		test, err = strconv.ParseBool(os.Args[3])
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	fmt.Printf("Solving challenge for day %d! Test input = %t\n", day, test)
+	fmt.Printf("Solving challenge %d for day %d! Test input = %t\n", part, day, test)
 
-	challenges.Day1(utils.FileToStringSlice(day, test))
+	challengePicker(day, part, test)
+}
+
+func challengePicker(day int, part int, test bool) {
+
+	switch day {
+	case 1:
+		if part == 1 {
+			challenges.Day1Part1(utils.FileToStringSlice(day, test))
+		} else if part == 2 {
+			challenges.Day1Part2(utils.FileToStringSlice(day, test))
+		}
+	case 2:
+		if part == 1 {
+			challenges.Day2Part1(utils.FileToStringSlice(day, test))
+		} else if part == 2 {
+			challenges.Day2Part2(utils.FileToStringSlice(day, test))
+		}
+	default:
+		fmt.Println("No challenges for that day")
+	}
 }
