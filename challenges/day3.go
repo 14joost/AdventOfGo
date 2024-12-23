2aed0b31bacf7efc9871976e1b25cca106b051e4
joostse <joost.sessink@kramp.com>
2024-12-23 16:25:59 +0100

    Intermediate commit

---
+++ challenges/day3.go
@@ -0,0 +1,13 @@
+package challenges
+
+import "fmt"
+
+func Day3Part1(stringSlice []string) {
+	for i, line := range stringSlice {
+		fmt.Printf("Line %d: %s\n", i, line)
+	}
+}
+
+func Day3Part2(stringSlice []string) {
+	// TODO
+}
